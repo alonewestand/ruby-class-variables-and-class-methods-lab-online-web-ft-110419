e23e5af51be2873c7a6c266d4eeffcae1b406212
Ken Goad <goadke@g.cofc.edu>
2019-11-15 16:07:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,3 +3,27 @@ attr_accessor :name, :artist, :genre
 @@count = 0  
 @@genres = [] 
 @@artists = []	
+
+def initialize (name, artist, genre)
+ @name = name 	
+  @artist = artist
+	  @genre = genre
+ @@count +=1	
+  @@genres << genre 
+ @@artists << artist 
+ 	end  
+def self.count 
+end          #self.name.uniq.count didn't work	/#returns array total number of unique songs
+
+def self.genres
+	@@genres.uniq 	
+end
+def self.artists
+	@@artists.uniq 
+end
+
+def
+end
+def
+end  	
+end
