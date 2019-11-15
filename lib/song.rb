34cd2396dd3893abdd9633989d89d5031441b6c9
Ken Goad <goadke@g.cofc.edu>
2019-11-15 17:33:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ attr_accessor :name, :artist, :genre
   def self.genre_count
     @@genres.each do |genre|
       if genres.include?genre then
-        genre_count = {genre => genre.count +=1}
+        genre_count = {genre => genre.count}
       end
     end
   end
