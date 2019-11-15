72f9c91ff0038ed39113ff5223904254d50a4218
Ken Goad <goadke@g.cofc.edu>
2019-11-15 18:09:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,12 @@ attr_accessor :name, :artist, :genre
 
 # iterate over the @@genres array and populate a hash with the key/value pairs. You will need to check to see if the hash already contains a key of a particular genre. If so, increment the value of that key by one, otherwise, create a new key/value pair.
 
+# #group_by → an_enumerator
+# Groups the collection by result of the block. Returns a hash where the keys are the evaluated result from the block and the values are arrays of elements in the collection that correspond to the key.
 
+# If no block is given an enumerator is returned.
+
+(1..6).group_by { |i| i%3 }   #=> {0=>[3, 6], 1=>[1, 4], 2=>[2, 5]}
   def self.genre_count
    @@genres.each_cons("rap" || "pop" || "rock")
     genre_baby.each do |genre|
