07171f2a97fbde70ad08b6f21f10a6a94a2cb110
Ken Goad <goadke@g.cofc.edu>
2019-11-15 16:06:49 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,34 +1,5 @@
 class Song
-
-  def self.count
-    self.uniq
-  end
-
-  def self.genres
-    @@genres.uniq
-  end
-
-  def self.artists
-    @@artists.uniq
-  end
-
-  def
-    self.genre_count
-    return hash.new("{#{@@genres.each}" => "{#{@@genres.each.count}")
-  end
-
-  attr_accessor :name,:artist,:genre
-
-  @@count = 0
-  @@genres = []
-  @@artists = []
-  
-  def initialize(name="Sweet Mo Time City River Holiday Blues Part Nine", artist="Ted", genre="Psychadelic")
-    @@count +=1
-    @@genres << genre
-    @@artists << artist
-  end
-  
-    hit_me.name = "hit me baby one more time" =
-
-end
+attr_accessor :name, :artist, :genre	
+@@count = 0  
+@@genres = [] 
+@@artists = []	
