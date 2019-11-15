c1b001440b2465c5f70fa9d2c60f794826b48463
Ken Goad <goadke@g.cofc.edu>
2019-11-15 18:11:37 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,7 +39,7 @@ attr_accessor :name, :artist, :genre
 # (1..6).group_by { |i| i%3 }   #=> {0=>[3, 6], 1=>[1, 4], 2=>[2, 5]}
 
   def self.genre_count
-   @@genres.group_by("rap" || "pop" || "rock")
+   (@@genres).group_by { |g| g.count}
     end
   end
 
